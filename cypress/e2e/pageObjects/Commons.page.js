d4e727c23d55710332d0bc6ede5c301dadcedb93
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2024-04-08 20:50:37 -0300

    Add login function to Commons.page.js

---
+++ cypress/e2e/pageObjects/Commons.page.js
@@ -0,0 +1,12 @@
+
+import { loginElements } from "../elements/elements.js";
+
+commonPage = {
+
+    login: () => {
+        cy.visit(`${Cypress.config("baseUrl")}`);
+        cy.get(loginElements.username).click().type(Cypress.env("USERNAME"));
+        cy.get(loginElements.password).type(Cypress.env("SENHA"));
+        cy.get(loginElements.submitButton).click();
+    }
+};
